98b390e08bf72332fff59c9f84068246ae7a4735
Gang Fan <gang.fan@uconn.edu>
2016-04-04 20:03:12 -0400

    Modify the madLib.js

--- client/madLib.js
+++ client/madLib.js
@@ -1,8 +1,17 @@
 Template.body.events({
-	'submit form': function(event) {
+	'submit form': function(event, template) {
 
 		event.preventDefault();
 
+		let exclamation = template.find('#exclamation').value;
+		let adverb = template.find('#adverb').value;
+		let animal = template.find('#animal').value;
+		let verb = template.find('#verb').value;
+
+
 		console.log('Hello, world');
+
+		console.log( exclamation + '!  you say, as you ' + adverb 
+			+' jump onto your '+ animal+' and '+ verb +' into the sunset.'  );
 	}
 });
